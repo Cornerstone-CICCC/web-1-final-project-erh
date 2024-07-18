e2894958d9bc7f835f3159210b30ed21a71dc956
Evangelina Vega <evangelina1001@hotmail.com>
2024-07-18 00:47:07 -0700

    Finished: Mobile design

---
+++ js/menu.js
@@ -0,0 +1,18 @@
+const menu = document.querySelector(".toggle-menu")
+const menuLogo = document.getElementById("message-logo")
+
+
+function openmenu(){
+    menu.style.opacity = "100%"
+    menuLogo.style.display = "none"
+}
+
+
+
+// function closemenu(){
+//     insta.style.display = "none"
+//     phone.style.display = "none"
+//     wha.style.display = "none"
+//     close.style.display = "none"
+//     messageIcon.style.display = "block"
+// }
