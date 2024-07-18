c821b940d0480e3456ffb1fe4fe6542407664025
Hiroki Ueno <rockyueno@gmail.com>
2024-07-18 15:34:48 -0700

    added header to html

--- js/index.js
+++ js/index.js
@@ -1,3 +1,7 @@
+fetch("partials/header.html")
+  .then((response) => response.text())
+  .then((data) => document.querySelector("#header").innerHTML = data);
+
 function toggleMenu() {
   const header = document.querySelector('.header');
   header.classList.toggle('menu-active');
